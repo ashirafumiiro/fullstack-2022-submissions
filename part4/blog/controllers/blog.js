8d4bce33620589c668580b5c0e6a935dbeb96b62
Miiro Ashirafu <ashirafumiiro@gmail.com>
2022-08-07 17:21:54 +0300

    completed 4.14

--- part4/blog/controllers/blog.js
+++ part4/blog/controllers/blog.js
@@ -2,22 +2,36 @@ const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
+
+blogsRouter.post('/', async (request, response) => {
+  const blog = new Blog(request.body)
+
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const res = await Blog.findByIdAndRemove(request.params.id)
+  if(!res) return response.status(404).end()
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = {
+    likes: body.likes
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
+  if(!updatedBlog) return response.status(404).end()
   
-  blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-  })
-  
-  module.exports = blogsRouter;
+  response.json(updatedBlog)  
+})
+
+
+module.exports = blogsRouter;
---
+++ part4/blog/tests/blog_api.test.js
@@ -0,0 +1,198 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+
+const api = supertest(app);
+const Blog = require('../models/blog');
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  console.log('cleared')
+
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+
+}, 1000000)
+
+
+describe('retrieving blogs', () => {
+  test('all blogs are returned in json', async () => {
+    const response = await api.get('/api/blogs').expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('returned blogs have id property defined', async () => {
+    const response = await api.get('/api/blogs').expect(200);
+
+    expect(response.body[0].id).toBeDefined();
+  });
+
+})
+
+describe('Adding a blog', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+    }
+
+    var response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(r => r.title)
+    expect(titles).toContain('Canonical string reduction');
+    let returnedBlog = response.body;
+    delete returnedBlog.id;
+    expect(response.body).toEqual(newBlog);
+  })
+
+  test('missing likes defaults to 0', async () => {
+    const newBlog = {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    }
+
+    var response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    expect(response.body.likes).toBe(0)
+  })
+
+  test('missing title raises 400', async () => {
+    const newBlog = {
+      author: 'Mr Ashirafu',
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      like: 3
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  })
+
+  test('missing url raises 400', async () => {
+    const newBlog = {
+      title: "Migrating to mongodb",
+      author: 'Mongo monkey',
+      like: 3
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  })
+
+
+})
+
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(
+      helper.initialBlogs.length - 1
+    )
+
+    const titles = blogsAtEnd.map(r => r.title)
+
+    expect(titles).not.toContain(blogToDelete.title);
+  })
+
+  test('raises 400 if id is mulformed', async () => {
+
+    await api
+      .delete(`/api/blogs/worong_id`)
+      .expect(400)
+  })
+
+  test('returns 404 for non existing id', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    // delete first time.
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    // delete second time.
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(404)
+
+  })
+
+})
+
+describe('updating a blog', () => {
+  test('succeeds with status code 200 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
+    const newLikesValue = 23;
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send({likes: newLikesValue})
+      .expect(200)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    const blogAfterUpdate = blogsAtEnd.find(blog => blog.id == blogToUpdate.id);
+
+    expect(blogAfterUpdate.likes).toBe(newLikesValue);
+
+  })
+
+  test('fails with status code 400 if id is malformatted', async () => {
+    const newLikesValue = 23;
+    await api
+      .put(`/api/blogs/bad_id`)
+      .send({likes: newLikesValue})
+      .expect(400)
+  })
+
+  test('fails with 404 if blog does not exist', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
+    
+    // deleting the blog
+    await api
+    .delete(`/api/blogs/${blogToUpdate.id}`)
+    .expect(204)
+
+    const newLikesValue = 23;
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send({likes: newLikesValue})
+      .expect(404)
+
+  })
+
+})
+
+
+afterAll(() => {
+  mongoose.connection.close()
+})
